a4ff27887617e563182b2011f000ed651c46d7c4
nhung179 <trinhnhung971@gmail.com>
2024-06-19 10:58:54 +0700

    BaoCao

---
+++ userpassword.py
@@ -0,0 +1,26 @@
+import paramiko
+
+hostname = "127.0.0.1"
+port = 22
+user = "kali"
+passwd = "kali"  
+
+try:
+    client = paramiko.SSHClient()  
+    client.load_system_host_keys()  
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, port=port, username=user, password=passwd)
+
+    while True:
+        try:
+            cmd = input("$>")
+            if cmd == "exit":
+                break
+            stdin, stdout, stderr = client.exec_command(cmd)  
+            print(stdout.read().decode())
+        except KeyboardInterrupt:
+            break
+    client.close()
+
+except Exception as err:
+    print(str(err))
